48260c29c89d0e8ed5b4da235063017d66369f28
AlviAhmed <alviahmed1234@gmail.com>
2019-01-13 03:56:50 -0500

    updated interrupt

--- main.c
+++ main.c
@@ -3,12 +3,24 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>  
-#define butpress (~PINB) & (1 << PB3)
+#define butpress (~PINB) & (1 << PB3) 
+int duty = 10;
 int main (void) { 
 	init();
     	while (1)  
-        { 	      
-
+        { 	       
+		/*_delay_ms(10);
+		if (butpress){  
+			_delay_ms(10);
+			if (butpress){ 
+				DDRB &=~ (1 << PB4);
+			}
+		} 
+		else {  
+			DDRB |= (1 << PB4);
+		
+		}
+*/
     	}  
 
 
@@ -22,29 +34,28 @@ void init(){
 	PORTB |=  (1 << PB3);    
 	GIMSK |= (1 << PCIE); 
 	PCMSK |= (1 << PCINT3); 
-	sei();
-	/*
 	GTCCR |= ((1 << COM1B1) | (1 << PWM1B));  //PB4 enabled for output of pwm
 	TIMSK |= (1 << TOIE1); 
 	sei(); 
 	TCCR1 |= (1 << CS10) | (1 << CS12) | (1 << CS11); //prescaler of 64 
-	*/
+
 } 
 
-/*
+
 ISR(TIMER1_OVF_vect){ 
 
-	OCR1B = 250;
+	OCR1B = duty;
 
 }  
-*/ 
+ 
 
 ISR(PCINT0_vect){ 
 	_delay_ms(10);
 	if (butpress)	{  
-		PORTB &=~ (1 << PB4);
+		DDRB &=~ (1 << PB4);
 	}
 	else {  
-		PORTB |= (1 << PB4);
+		DDRB |= (1 << PB4);
 	}
-}
+} 
+
