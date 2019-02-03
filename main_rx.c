8c4a76d903a2e8e63d526480471b8ccbbdb1d2a3
AlviAhmed <alviahmed1234@gmail.com>
2019-02-03 13:30:10 -0500

    resolved low pin voltage issue, states working fine

--- main_rx.c
+++ main_rx.c
@@ -10,7 +10,9 @@
 #define butpress (PINB) & _BV(2)
 int tick_count = 0;
 int enable = 0;   
-int state = 0;
+int state = 0; 
+
+//Either something wrong with defining ports 
 
 int main (void) {  
 	init();  
@@ -20,33 +22,32 @@ int main (void) {
 		//PORTB = PB7 PB6 PB5 PB4 PB3 PB2 PB1 PB0  
 		//PB2 => input for signal 
 		//led ports => PB4, PB3, PB1, PB0  
-
+		_delay_ms(5);
 		if (~butpress){ 
 			//State 4 FWD
 			if (tick_count > 14 && tick_count <= 18){  //when tick_counter = 16.1, means 1.601 ms gone by 
 			//	PORTB = PORTB & (0x00) | (0x10); //turning off all leds other than PB4  
 				PORTB = PORTB & (0x00) | (0x12);//just PB4 & PB1 
 			}   
-			//State 3 REV	
+			//State 3 RIGHT
 			else if (tick_count > 10 && tick_count <= 13){ //tick counter = 12.04 
 				//PORTB = PORTB & (0x00) | (0x08); // //   ///               //     PB3 
-				  PORTB = PORTB & (0x00) | (0x09);//PB3 & PB0
+				  PORTB = PORTB & (0x00) | (0x11);//PB3 & PB0
 			}  
 			//State 2 LEFT
 			else if (tick_count > 6 && tick_count <= 10){ //tick counter = 8.0314
 				//PORTB = PORTB & (0x00) | (0x02); // //   ///               //     PB1 
 				PORTB = PORTB & (0x00) | (0x0A);//PB3 & PB1
 			} 
-			//State 1 RIGHT
+			//State 1 REV
 			else if (tick_count > 2 && tick_count <= 6){ //tick counter = 4.0
 				//PORTB = PORTB & (0x00) | (0x01); // //   ///               //     PB0 
-				PORTB = PORTB & (0x00) | (0x11);//PB0 & PB4
+				PORTB = PORTB & (0x00) | (0x09);//PB0 & PB4
 			} 
 			//State 0 OFF
 			else if (tick_count <= 1) { //tick counter = 0.8 (default signal)
-				PORTB &=~ (0xFF);  //turn of all bits
+				PORTB &=~ (0x1B);  //turn of all bits
 			}     
-			tick_count = 0; //setting tick count back to zero after pulse hits low 
 		}
     	}  
 
@@ -59,8 +60,8 @@ void init(){
 	DDRB &=~ (1<<PB2);  //setting PB4(ADC2) as input for the RX signal     
 
 	// ******* led init
-	DDRB |= (0x1B);  //setting up leds
-	PORTB &=~ (0x1B); //initially turning all leds off  
+	DDRB |= (0x1B);  //setting up leds expect for PB2
+	PORTB &=~ (0x1B); //initially turning all leds off expect for PB2
 
 	//*********INT0 init	
 	MCUCR |=/* (1 << ISC01) |*/ (1 << ISC00);//ISC01 & ISC00 both 1, triggers INT0 on rising edge 
@@ -74,7 +75,8 @@ void init(){
 }   
 
 ISR(INT0_vect){    
-	if (butpress) { 
+	if (butpress) {  
+		tick_count = 0;
 		TCCR0B |= (1 << CS00); //turning on the timer, when pulse is high       
 	} 
 	else { 
