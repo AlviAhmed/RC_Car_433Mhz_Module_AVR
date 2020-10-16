563ec65ebfb5d38bdab837231206fc71682598f7
AlviAhmed <alviahmed1234@gmail.com>
2020-10-15 21:22:44 -0400

    usart working with rx and tx buffers

--- avr_usart/mainrx.c
+++ avr_usart/mainrx.c
@@ -18,21 +18,58 @@
 #define pin PB0
 #define BAUD 9600 //same rate as in the Makefile baud is in bits per second
 #define ubbrn ((F_CPU/16/BAUD) - 1)
-#define buffer_size 128
 
-#define syncByte 0xAA
-#define rxSerialNum1 0x0C
 
-char rxbuffer[buffer_size];
-uint8_t rxreadpos = 0;
-uint8_t rxwritepos = 0;
+#define bufferSize 128
+uint8_t rxBuffer[bufferSize];
+uint8_t rxWritePos = 0;
+uint8_t rxReadPos = 0;
 
-char ar[128]= " hello ";
+uint8_t  rxSerialNum1 = 0xCF;
+uint8_t rxCmdRec = 0x00;
+uint8_t rxSyncRec = 0x00;
+uint8_t rxSerRec = 0x00;
+uint8_t rxBlkRec = 0x00;
+
+uint8_t syncByte = 0xAA;
+
+
 uint8_t i = 0;
 int enable = 1;
 char c = '\0';
 
-volatile uint8_t rxByte = 0x00;
+volatile uint8_t rxByte;
+
+
+
+
+uint8_t recieveByte(){
+    while( ((UCSR0A & (1<<RXC0)) == 0) && (UDR0 == 0x00) );
+  return UDR0;
+}
+
+void appendRx(uint8_t data_byte) //UDR0 will be the input of the function
+{
+    rxBuffer[rxWritePos] = data_byte;
+    rxWritePos++; //increment global write position as you are inputting things into the array
+
+    if (rxWritePos >= bufferSize){
+        rxWritePos  = 0;
+    }
+}
+
+void readRxCmdFunc(){
+    rxCmdRec = 0x00;
+    if (rxReadPos != rxWritePos){
+        rxCmdRec = rxBuffer[rxReadPos];
+        rxReadPos ++;
+        if (rxReadPos >= bufferSize){
+            rxReadPos = 0;
+        }
+    }
+}
+
+
 
 int main(void){
     DDRB |= (1 << PB5) | (1 << PB4) | (1 << PB3) | (1 << PB2) | (1 << PB1);
@@ -51,37 +88,40 @@ int main(void){
     ////////////////////////////////////////////////////////////////
     UDR0 = 0;
     while (1){
-
-        if (rxByte == rxSerialNum1){
-            addr = rxByte;
-            if (addr == 0x04)
+        // readRxCmdFunc();
+            if (rxCmdRec == 0x44)
             { 
                 PORTB |= (1 << PB5);
             }
-            else if (addr == 0x03)
+            else if (rxCmdRec == 0xCB)
             {
                 PORTB |= (1 << PB4);
             }
-            else if (addr == 0x02)
+            else if (rxCmdRec == 0x62)
             {
                 PORTB |=  (1 << PB3);
             }
-            else if (addr == 0x01)
+            else if (rxCmdRec == 0xFA)
             {
                 PORTB |= (1 << PB2);
             }
-            else if (addr == 0x05){ 
+            else if (rxCmdRec == 0x05){ 
                 PORTB &=~ (1 << PB5);
                 PORTB &=~ (1 << PB4);
                 PORTB &=~ (1 << PB3);
                 PORTB &=~ (1 << PB2);
             }
         }
-        }
 }  
 
 ISR(USART_RX_vect)
 {
-    rxByte = UDR0;
+    // uint8_t rxByte;
+    rxCmdRec = UDR0;
+    // uint8_t blankAddr, syncAddr, serAddr, cmdAddr ;
+    // blankAddr = recieveByte();
+    // cmdAddr = recieveByte();
+    // appendRx(blankAddr);
+    // appendRx(cmdAddr);
 }
 
--- avr_usart/maintx.c
+++ avr_usart/maintx.c
@@ -25,21 +25,56 @@
 #define but1 (~PINB) & (1 << PB3) // PCINT3
 #define but0 (~PINB) & (1 << PB2) // PCINT2
 
-#define syncByte 0xAA
-#define rxSerialNum1 0x0C
 #define rxSerialNum2 0x0C
+#define bufferSize 128
+
+/* NOTE: since I am sending 2 bytes of info, 1 per index, that means that per transmission  
+2 positions in the array will be taken and transmitted. 
+
+For now this works since 128 is a multiple of 2, therefore when the array (readpos / write pos) eventually 
+overlaps / ends there will be no overlaping of data. 
+
+HOWEVER, if the amount of commands being send per packet should change (i.e. 3 or 5 commands). Then the buffer size HAS to be 
+a multiple to avoid overriding values in the array when resetting the index(readpos / write pos). 
+
+ */
+uint8_t syncByte = 0xAA;
+uint8_t blankByte = 0x05;
+uint8_t  rxSerialNum1 = 0xCF;
+
+uint8_t txBuffer[bufferSize];
+uint8_t txWritePos = 0;
+uint8_t txReadPos = 0;
+
+
+
 
 
 volatile int pressed = 0;
-volatile char ar = 'n';
 volatile uint8_t txByte = 0x00;
 uint8_t i = 0;
 int enable = 1;
 
-void txPacket(uint8_t rxbyte, uint8_t command){
-    txByte = syncByte;
-    txByte = rxbyte;
-    txByte = command;
+
+void nullByteIfEmpty(){
+    if (UCSR0A & (1 << UDRE0)) { //if the UDRE register is empty, then send a null byte, just to make sure no junk is put in to the register
+        UDR0 = 0x00;
+     }
+}
+void appendTx(uint8_t data_byte)
+{
+    txBuffer[txWritePos] = data_byte;
+    txWritePos++; //increment global write position as you are inputting things into the array
+
+    if (txWritePos >= bufferSize){
+        txWritePos  = 0;
+    }
+}
+
+void txPacket(uint8_t command){
+    appendTx(blankByte);
+    appendTx(command);
+    nullByteIfEmpty(); //set null byte if UDRE is empty after sending all the commands
 }
 
 
@@ -65,6 +100,7 @@ int main(void){
     sei();
     
     while (1){
+
     }
 }  
 
@@ -72,7 +108,7 @@ int main(void){
 ISR(PCINT0_vect){
     _delay_ms(5); 
     if (but3){
-        txPacket(rxSerialNum1,0x04);
+        txPacket(0x44);
         // txByte = syncByte;
         // txByte = 0x04;
         // ar = '3';
@@ -80,7 +116,7 @@ ISR(PCINT0_vect){
     }
         
     else if (but2){    
-        txPacket(rxSerialNum1,0x03);
+        txPacket(0xCB);
         // txByte = syncByte; 
         // txByte = 0x03;
         // ar = '2';
@@ -88,7 +124,7 @@ ISR(PCINT0_vect){
     }
         
     else if (but1){
-        txPacket(rxSerialNum1,0x02);
+        txPacket(0x62);
         // txByte = syncByte; 
         // txByte = 0x02;
        // ar = '1';
@@ -96,7 +132,7 @@ ISR(PCINT0_vect){
     }
         
     else if (but0){    
-        txPacket(rxSerialNum1,0x01);
+        txPacket(0xFA);
         // txByte = syncByte; 
         // txByte = 0x01;
         // ar = '0';
@@ -104,7 +140,7 @@ ISR(PCINT0_vect){
     }
         
     else {   
-        txPacket(rxSerialNum1,0x05);
+        txPacket(0x05);
         // txByte = syncByte; 
         // txByte = 0x05;
         // ar = 'n';
@@ -115,7 +151,13 @@ ISR(PCINT0_vect){
 
 ISR(USART_TX_vect)
 {
-    // UDR0 = ar;
-    UDR0 = txByte;
+    // UDR0 = 0x00;
+    if (txReadPos != txWritePos){ //now we are reading the info from the buffer index by index to be transmitted
+        UDR0 = txBuffer[txReadPos]; //tx read pos just used to index the buffer data to be transmitted
+        txReadPos ++;
+        if (txReadPos >= bufferSize){
+            txReadPos = 0;
+        }
+    }
 }
 
